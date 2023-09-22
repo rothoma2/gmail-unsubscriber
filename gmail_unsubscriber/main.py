92992ed43f508b84c6b3a159392f0708b070f96e
robertthomas <thomcrnl@gmail.com>
2023-09-22 23:09:31 +0200

    Progress on UI Switching

--- gmail_unsubscriber/main.py
+++ gmail_unsubscriber/main.py
@@ -2,6 +2,7 @@ import typing
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, uic
 from PyQt5.QtWidgets import QWidget
+from functools import partial
 import sys
 import os
 
@@ -10,26 +11,52 @@ if getattr(sys, 'frozen', False):
 else:
     RELATIVE_PATH = os.path.dirname(__file__)
 
+def attempt_login(MainForm) -> None:
+    email = MainForm.txt_email.text()
+    password = MainForm.txt_password.text()
+    if email == "user" and password == "pass":
+        print("Login Success")
+    MainForm.unsubscribe_form.show()
+    MainForm.hide()
 
-class MainGui(QMainWindow):
+
+class MainForm(QMainWindow):
 
     def __init__(self) -> None:
-        super(MainGui, self).__init__()
+        super(MainForm, self).__init__()
         self._ui_path = RELATIVE_PATH
         uic.loadUi(os.path.join(self._ui_path, 'login_form.ui'), self)
+        self.unsubscribe_form = UnsubscribeForm(self)
+        self.unsubscribe_form.hide()
+        #self.btn_login.clicked.connect(self.btn_login_clicked)
+        login_func = partial(attempt_login, self)
+        self.btn_login.clicked.connect(login_func)
         self.show()
         self.setFixedSize(self.size())
 
+    def btn_login_clicked(self):
+        email = self.txt_email.text()
+        password = self.txt_password.text()
 
-def check_credentials(self):
-    # For demonstration purposes, let's assume the correct credentials are:
-    # Username: admin
-    # Password: password123
-    pass
+class UnsubscribeForm(QDialog):
+
+    def __init__(self, parent) -> None:
+        super(UnsubscribeForm, self).__init__()
+        self._ui_path = RELATIVE_PATH
+        uic.loadUi(os.path.join(self._ui_path, 'unsubscribe_form.ui'), self)
+        self.hide()
+
+    def btn_login_clicked(self):
+        email = self.txt_email.text()
+        password = self.txt_password.text()
+        attempt_login(email, password)
+
+
+
 
 def main():
     app = QApplication([])
-    window = MainGui()
+    window = MainForm()
     app.exec_()
 
 if __name__ == "__main__":
